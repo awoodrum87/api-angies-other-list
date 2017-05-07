aca7eaea04da117ac8467c27c04c25e0b1ad5080
awoodrum87 <awoodrum87@gmail.com>
2017-05-07 16:27:39 -0400

    modified models to block a null response

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 # this code works to create and update a reviewer. Edit with caution
 class Review < ApplicationRecord
   belongs_to :reviewer
+# add validates props to to the model
+  validates :review_date, presence: true
+  validates :review_text, presence: true
 end
--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -3,7 +3,13 @@
 #   belongs_to :user
 # end
 
+# add validates props to to the model
+# validates :field, presence true
 class Reviewer < ApplicationRecord
   belongs_to :user
   has_many :reviews
+
+  validates :username, presence: true
+  validates :industry, presence: true
+
 end
