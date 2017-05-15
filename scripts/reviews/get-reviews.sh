2f9812c79eb064d86663b1f13f6d44a562539640
awoodrum87 <awoodrum87@gmail.com>
2017-05-14 22:18:22 -0400

    updates curl request

--- scripts/reviews/get-reviews.sh
+++ scripts/reviews/get-reviews.sh
@@ -2,9 +2,18 @@
 
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/reviews"
+TOKEN="BAhJIiUyZDIxY2RkYWEzY2MxY2U1MDg1NTc3Nzg2ODllYjViZQY6BkVG--7c77c92f6dab9bc9a2b6ee4690a33f35f0aa1464"
 curl "${API}${URL_PATH}" \
   --include \
   --request GET \
   --header "Authorization: Token token=$TOKEN"
 
 echo
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/your-reviews"
+TOKEN="BAhJIiUyZDIxY2RkYWEzY2MxY2U1MDg1NTc3Nzg2ODllYjViZQY6BkVG--7c77c92f6dab9bc9a2b6ee4690a33f35f0aa1464"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=$TOKEN"
